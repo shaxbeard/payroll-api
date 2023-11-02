cc4d2fafc6457747d8978a582ab5d1a00a90dcda
Eric Carlson <110621411+ericandrewcarlson@users.noreply.github.com>
2023-11-02 10:46:34 -0400

    Move Multer logic into the upload controller

--- routes/upload.js
+++ routes/upload.js
@@ -1,24 +1,11 @@
 const express = require("express");
-const multer = require("multer");
-const uuid = require("uuid");
 const UploadController = require("../controllers/UploadController");
-
 const router = express.Router();
 
-//Config Multer for file uploads
-const storage = multer.diskStorage({
-  // destination: "../uploads/",
-  destination: function (req, file, cb) {
-    cb(null, "../uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, "time-report-" + uuid.v4() + ".csv");
-  },
-});
-
-// Initialize upload
-const upload = multer({ storage: storage });
-
-router.post("/upload", upload.single("csvFile"), UploadController.uploadCSV);
+router.post(
+  "/upload",
+  UploadController.uploadMiddleware,
+  UploadController.uploadCSV
+);
 
 module.exports = router;
